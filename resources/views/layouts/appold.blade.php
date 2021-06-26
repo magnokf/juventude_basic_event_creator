ebd0911cb77948ed41408c062ce4fd792f50e222
magnokf <mkfelipe.rj@gmail.com>
2021-06-26 12:22:06 -0300

    blue color on top browser

--- resources/views/layouts/appold.blade.php
+++ resources/views/layouts/appold.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="theme-color" content="#7BDFFB">
+    <meta name="theme-color" content="#06BBF0">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
