215d410f89cbc6b9d3bceecec4a7d63cc7417583
magnokf <mkfelipe.rj@gmail.com>
2021-06-28 21:45:56 -0300

    fix web

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -61,9 +61,9 @@
             <p class="mt-3 mb-1">
                 <a href="{{ route("login") }}">Login</a>
             </p>
-            <p class="mb-0">
-                <a href="{{ route("register") }}" class="text-center">Registrar novo membro</a>
-            </p>
+{{--            <p class="mb-0">--}}
+{{--                <a href="{{ route("register") }}" class="text-center">Registrar novo membro</a>--}}
+{{--            </p>--}}
         </div>
         <!-- /.login-card-body -->
     </div>
