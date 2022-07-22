1abc0193d805fc18c7c7d43365b35b898d72b1e6
magnokf <mkfelipe.rj@gmail.com>
2022-07-22 11:34:53 -0300

    feat: inicia a implementação de criação de evento

---
+++ database/migrations/2022_07_20_063752_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 150);
+            $table->dateTime('date_of_event');
+            $table->dateTime('deadline_of_event');
+            $table->string('location', 150)->nullable();
+            $table->string('description', 500)->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
